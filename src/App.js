ac85c2f07c2ceadbabe4eb94ca8f497ba70f4963
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2019-08-02 21:55:40 -0500

    Project refactor "Move styles back to react and put static files in public folder, add i18n package to hanlde languages"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { withNamespaces } from 'react-i18next';
 import LoginContainer from './views/login/LoginContainer';
 
 class App extends Component {
   render() {
+    const { t } = this.props;
     return (
       <div className="App container-fluid">
         <LoginContainer/>
+        <h1>{t('Welcome to React')}</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default withNamespaces()(App);
